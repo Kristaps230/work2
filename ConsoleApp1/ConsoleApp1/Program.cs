db212f276774b3c43bc6685fe91d1648d018b92c
Kristaps230 <90153701+Kristaps230@users.noreply.github.com>
2021-09-14 21:06:14 +0300

    change

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ka tevi sauc?");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Sveiks, " + userName);
+
+
+            //---------------------------------------------------------
+
+
+            
+            
+
+            Console.WriteLine("Kads ir tavs vecums?");
+            string ageText = Console.ReadLine();
+            int age = int.Parse(ageText);
+            int nextYearAge = age + 1;
+
+            Console.WriteLine("Nakamgad tev paliks " + nextYearAge + ", Tu esi pilngadigs");
+
+
+
+            //--------------------------------------------------------
+
+            Console.WriteLine("===================================================");
+
+            int maxNumber4 = 9;
+            int maxNumber5 = 20;
+
+            int maxResult = Math.Max(maxNumber4, maxNumber5);
+
+            Console.WriteLine(maxResult);
+
+
+            //------------------------------------------------------
+
+            Console.WriteLine("=====================================================");
+
+            int minNumber6 = 15;
+            int maxNumber7 = 30;
+
+            int minResult = Math.Min(minNumber6, maxNumber7);
+
+            Console.WriteLine(minResult);
+
+
+            //------------------------------------------------------
+
+            Console.WriteLine("===================================================");
+
+            int divisionremainder3 = 16;
+
+            int divisionremainder4 = 4;
+
+            int divisionremainderResult = divisionremainder3 % divisionremainder4;
+
+            Console.WriteLine(divisionremainderResult);
+
+
+            //-----------------------------------------------------
+
+
+            
+
+          
+            
+
+
+
+            
+
+
+
+
+
+
+
+                
+
+         
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+            
+
+
+
+
+
+
+
+        }
+
+
+        
+    }
+}
